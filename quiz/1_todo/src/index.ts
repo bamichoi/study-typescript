97c797a01aef5b0a83f9a63caccd2ce5bbf24d0e
beomseok.choi <cbs412@naver.com>
2022-03-09 10:05:12 +0900

    interface

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,13 +1,14 @@
-
-type Todo = {
-  id: number, title:string, done:boolean
+interface Todo {
+  id: number; 
+  title: string; 
+  done: boolean;
 }
 
 let todoItems : Todo[];
 
 
 // api
-function fetchTodoItems() : {id: number, title: string, done:boolean}[] {
+function fetchTodoItems() : Todo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -22,7 +23,7 @@ function fetchTodos() : object[] {
   return todos;
 }
 
-function addTodo(todo : {id: number, title:string, done:boolean}) : void {
+function addTodo(todo : Todo) : void {
   todoItems.push(todo);
 }
 
@@ -30,7 +31,7 @@ function deleteTodo(index : number) {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index : number, todo : {id: number, title:string, done:boolean}) : void {
+function completeTodo(index : number, todo : Todo) : void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
