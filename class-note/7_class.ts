d75ab1d83bc872457b22d8d7fdb1aad8ac113130
beomseok.choi <cbs412@naver.com>
2022-03-13 21:26:05 +0900

    class

--- class-note/7_class.ts
+++ class-note/7_class.ts
@@ -1,6 +1,7 @@
 class Person {
-  name: string;
-  age: number;
+  private name: string;
+  public age: number;
+  readonly log: string;
 
   constructor(name: string, age: number) {
     this.name = name;
