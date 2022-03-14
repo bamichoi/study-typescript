9dbad4626d9d278039fc3eb9ae28cba24b5c65c6
beomseok.choi <cbs412@naver.com>
2022-03-14 11:20:22 +0900

    generics

--- class-note/7_class.ts
+++ class-note/7_class.ts
@@ -1,10 +1,29 @@
 class Person {
-  private name: string;
+  readonly name: string;
   public age: number;
-  readonly log: string;
 
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
 }
+
+let bami = new Person("bami", 100);
+
+class Developer {
+  private name: string;
+
+  get name(): string {
+    return this.name;
+  }
+  set name(newValue: string) {
+    if (newValue && newValue.length > 5) {
+      throw new Error("This name is too long");
+    }
+    this.name = newValue;
+  }
+}
+
+const bbami = new Developerr();
+bbami.name = "beomseok choi"; // Error
+bbami.name = "choi";
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,17 +1,49 @@
-const emails = [
-  { value: 'naver.com', selected: true },
-  { value: 'gmail.com', selected: false },
-  { value: 'hanmail.net', selected: false },
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+// 인터페이스를 이용하면 타입하나 바꾸기위해서 인터페이스 또 추가해야함. 타입이 늘어나는 만큼 인터페이스 계속 늘어나게 됨.
+
+interface Item<T> {
+  value: T;
+  selected: boolean;
+}
+
+const emails: Item<string>[] = [
+  { value: "naver.com", selected: true },
+  { value: "gmail.com", selected: false },
+  { value: "hanmail.net", selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: Item<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
-  const option = document.createElement('option');
+// function createDropdownItem(item: Item<string> | Item<number>) {
+//   const option = document.createElement("option");
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+// // NOTE: 이메일 드롭 다운 아이템 추가
+// emails.forEach(function (email) {
+//   const item = createDropdownItem(email);
+//   const selectTag = document.querySelector("#email-dropdown");
+//   selectTag.appendChild(item);
+// });
+
+function createDropdownItem<T>(item: Item<T>) {
+  const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
   option.selected = item.selected;
@@ -20,7 +52,11 @@ function createDropdownItem(item) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
+  const item = createDropdownItem<string>(email);
+  const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem(product);
 });
