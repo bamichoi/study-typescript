dea24bb01d78670f6bd2cb89b681ae15dc11d910
beomseok.choi <cbs412@naver.com>
2022-06-03 21:45:57 +0900

    Utility type

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -38,3 +38,58 @@ function displayProductDetail(
 ) {
   //..
 }
+
+// interface UpdateProduct {
+//   id?: number;
+//   name?: string;
+//   price?: number;
+//   brand?: string;
+//   stock? : number;
+// }
+
+// 3. 특정 상품 정보를 업데이트(갱신)하는 함수
+
+type UpdateProduct = Partial<Product>; // 옵셔널 한 것과 같은 효과
+
+function updateProductItem(productItem: Partial<Product>) {}
+
+// 4. 유틸리티 타입 구현 - Partial
+
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+
+// interface UserProfileUpdate {
+//   username?: string;
+//   email?: string;
+//   profilePhotoUrl?: string;
+// }
+
+// #1.
+// type UserProfileUpdate = {
+//   username?: UserProfile["username"];
+//   email?: UserProfile["email"];
+//   profilePhotoUrl?: UserProfile["profilePhotoUrl"];
+// };
+
+// #2. (mapped type)
+
+type UserProfileKeys = keyof UserProfile;
+
+type UserProfileUpdate = {
+  [p in "username" | "email" | "profilePhotoUrl"]?: UserProfile[p];
+};
+
+// #3.
+
+type UserProfileUpdate = {
+  [p in keyof UserProfile]?: UserProfile[p];
+};
+
+// #4 Partail. (제네릭 첨가)
+
+type Subset<T> = {
+  [p in keyof T]?: T[p];
+};
